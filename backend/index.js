b6e414e84dc2188fcfa3830305efe3b29d985007
fsRakib <fsrakiba50@gmail.com>
2024-05-31 13:03:19 +0600

    Login & Register UI intregrated with login & register API. Toast as notification implemented

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import { databaseConnection } from "./config/database.js";
 import cookieParser from "cookie-parser";
 import userRoute from "./routes/userRoute.js";
 import chitRoute from "./routes/chitRoute.js";
+import cors from "cors";
 
 dotenv.config({
   path: ".env",
@@ -20,6 +21,12 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser());
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 //api
 app.use("/api/v1/user", userRoute);
--- frontend/chit/src/components/Login.js
+++ frontend/chit/src/components/Login.js
@@ -1,7 +1,76 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { USER_API_END_POINT } from "../utils/constant";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
   const [isLogin, setIsLogin] = useState(true);
+
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (isLogin) {
+      //  login
+      try {
+        const res = await axios.post(
+          `${USER_API_END_POINT}/login`,
+          {
+            email,
+            password,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+            withCredentials: true,
+          }
+        );
+        console.log(res);
+        if (res.data.success) {
+          navigate("/");
+          toast.success(res.data.msg);
+        }
+      } catch (error) {
+        toast.success(error.response.data.msg);
+        console.log(error);
+      }
+    } else {
+      //Register
+      try {
+        const res = await axios.post(
+          `${USER_API_END_POINT}/register`,
+          {
+            name,
+            username,
+            email,
+            password,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+            withCredentials: true,
+          }
+        );
+        console.log(res);
+        if (res.data.success) {
+          setIsLogin(true);
+          toast.success(res.data.msg);
+        }
+      } catch (error) {
+        toast.success(error.response.data.msg);
+        console.log(error);
+      }
+    }
+  };
+
   const loginSignupHandler = () => {
     setIsLogin(!isLogin);
   };
@@ -24,35 +93,43 @@ export const Login = () => {
           <h1 className="mt-4 mb-2 text-2xl font-bold">
             {isLogin ? "Login" : "Register"}
           </h1>
-          <form className="flex flex-col w-[82%]">
+          <form onSubmit={submitHandler} className="flex flex-col w-[82%]">
             {!isLogin && (
               <>
                 <input
                   type="text"
                   placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                   className="outline-green-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold"
                 />
                 <input
                   type="text"
                   placeholder="Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
                   className="outline-green-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold"
                 />
               </>
             )}
 
             <input
-              type="text"
+              type="email"
               placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="outline-green-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold"
             />
             <input
-              type="text"
+              type="password"
               placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="outline-green-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold"
             />
 
             <button className="bg-[#39ff14] border-none py-2 my-4 rounded-full text-lg text-black">
-            {isLogin ? "Login" : "Create Account"}
+              {isLogin ? "Login" : "Create Account"}
             </button>
             <h1>
               {isLogin
