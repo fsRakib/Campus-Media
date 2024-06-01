64a33123269afae7153bc9c781e7eb2ee35baa82
fsRakib <fsrakiba50@gmail.com>
2024-06-02 01:30:30 +0600

    Like or dislike and delete UI for post integrated with likeOrDislike and deletePost API User can like any post and delete only their own post (Chit).

--- frontend/chit/src/components/Chit.js
+++ frontend/chit/src/components/Chit.js
@@ -1,10 +1,48 @@
 import React from "react";
 import Avatar from "react-avatar";
+import axios from "axios";
 import { TfiCommentsSmiley } from "react-icons/tfi";
 import { PiHandshakeThin } from "react-icons/pi";
 import { MdOutlineBookmarks } from "react-icons/md";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { CHIT_API_END_POINT } from "../utils/constant";
+import { useDispatch, useSelector } from "react-redux";
+import toast from "react-hot-toast";
+import { getRefresh } from "../redux/chitSlice";
 
-export const Chit = ({chit}) => {
+export const Chit = ({ chit }) => {
+  const { user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const likeOrDislikeHandler = async (id) => {
+    try {
+      const res = await axios.put(
+        `${CHIT_API_END_POINT}/like/${id}`,
+        { id: user?._id },
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(getRefresh());
+      toast.success(res.data.msg);
+    } catch (error) {
+      toast.success(error.response.data.msg);
+      console.log(error);
+    }
+  };
+
+  const deleteChitHandler = async (id) => {
+    try {
+      axios.defaults.withCredentials = true;
+      const res = await axios.delete(`${CHIT_API_END_POINT}/delete/${id}`);
+      console.log(res);
+      dispatch(getRefresh());
+      toast.success(res.data.msg);
+    } catch (error) {
+      toast.success(error.response.data.msg);
+      console.log(error);
+    }
+  };
+
   return (
     <div className="border-b border-gray-200">
       <div>
@@ -17,14 +55,19 @@ export const Chit = ({chit}) => {
           <div className="ml-2 w-full">
             <div className="flex items-center ">
               <h1 className="font-bold">{chit?.userDetails[0]?.name}</h1>
-              <p className="text-gray-500 text-sm ml-1">{`@${chit?.userDetails[0]?.username}`} . 1m</p>
+              <p className="text-gray-500 text-sm ml-1">
+                {`@${chit?.userDetails[0]?.username}`} . 1m
+              </p>
             </div>
             <div>
               <p>{chit?.description}</p>
             </div>
             <div className="flex justify-between my-3">
               <div className="flex items-center">
-                <div className="p-2 hover:bg-green-200 rounded-full cursor-pointer">
+                <div
+                  onClick={() => likeOrDislikeHandler(chit?._id)}
+                  className="p-2 hover:bg-green-200 rounded-full cursor-pointer"
+                >
                   <PiHandshakeThin size="24px" />
                 </div>
                 <p>{chit?.like?.length}</p>
@@ -41,6 +84,16 @@ export const Chit = ({chit}) => {
                 </div>
                 <p>0</p>
               </div>
+              {user?._id === chit?.userId && (
+                <div
+                  onClick={() => deleteChitHandler(chit?._id)}
+                  className="flex items-center"
+                >
+                  <div className="p-2 hover:bg-red-300 rounded-full cursor-pointer">
+                    <RiDeleteBin6Line size="24px" />
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
