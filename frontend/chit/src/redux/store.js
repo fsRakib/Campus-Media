ec6fb0192d439399100a4d173b5239965fbec461
fsRakib <fsrakiba50@gmail.com>
2024-06-02 20:25:40 +0600

    Persist user data using Redux-Persist Reloading the page , data will not vanish.

--- frontend/chit/src/redux/store.js
+++ frontend/chit/src/redux/store.js
@@ -1,12 +1,38 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
 import chitSlice from "./chitSlice";
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  user: userSlice,
+  chit: chitSlice,
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: {
-    //slice
-    user: userSlice,
-    chit: chitSlice,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 export default store;
